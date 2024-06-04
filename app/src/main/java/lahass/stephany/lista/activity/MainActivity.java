99fb5ff7a77dde5817005bc0b60122d6abc68d08
20201tiimi0292 <lahassstephany@gmail.com>
2024-06-04 11:16:34 -0300

    commit passo 7

---
+++ app/src/main/java/lahass/stephany/lista/activity/MainActivity.java
@@ -0,0 +1,51 @@
+package lahass.stephany.lista.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import lahass.stephany.lista.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        public class MainActivity extends AppCompatActivity {
+
+            static int NEW_ITEM_REQUEST =1;
+
+            @Override
+            protected void onCreate(Bundle savedInstanceState) {
+                super.onCreate(savedInstanceState);
+                setContentView(R.layout.activity_main);
+                //aqui comeca o botao FAB
+                FloatingActionButton fabAddItem =
+                        findViewById(R.id.fabAddNewItem);
+                //registra o ouvidor de clicks
+                fabAddItem.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent i = new Intent(MainActivity.this,
+                                NewItemActivity.class);
+                        startActivityForResult(i, NEW_ITEM_REQUEST);
+                    }
+                });
+    }
+}
--- app/src/main/java/lahass/stephany/lista/activity/NewItemActivity.java
+++ app/src/main/java/lahass/stephany/lista/activity/NewItemActivity.java
@@ -1,4 +1,4 @@
-package lahass.stephany.lista;
+package lahass.stephany.lista.activity;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import lahass.stephany.lista.R;
 
 public class NewItemActivity extends AppCompatActivity {
 
