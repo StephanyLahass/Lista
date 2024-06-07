9ad7ab8e83e79225ab838f78466d0cf2f1d4fd26
20201tiimi0292 <lahassstephany@gmail.com>
2024-06-07 09:12:05 -0300

    final do passo 13

--- app/src/main/java/lahass/stephany/lista/activity/MainActivity.java
+++ app/src/main/java/lahass/stephany/lista/activity/MainActivity.java
@@ -1,8 +1,11 @@
 package lahass.stephany.lista.activity;
 
+
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,9 +15,15 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import lahass.stephany.lista.R;
+import lahass.stephany.lista.model.MyItem;
 
 public class MainActivity extends AppCompatActivity {
+    static int NEW_ITEM_REQUEST =1;
+    List<MyItem> itens = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,25 +36,32 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
-        public class MainActivity extends AppCompatActivity {
-
-            static int NEW_ITEM_REQUEST =1;
-
+        //aqui comeca o botao FAB
+        FloatingActionButton fabAddItem = findViewById(R.id.fabAddNewItem);
+        //registra o ouvidor de clicks
+        fabAddItem.setOnClickListener(new View.OnClickListener() {
             @Override
-            protected void onCreate(Bundle savedInstanceState) {
-                super.onCreate(savedInstanceState);
-                setContentView(R.layout.activity_main);
-                //aqui comeca o botao FAB
-                FloatingActionButton fabAddItem =
-                        findViewById(R.id.fabAddNewItem);
-                //registra o ouvidor de clicks
-                fabAddItem.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        Intent i = new Intent(MainActivity.this,
-                                NewItemActivity.class);
-                        startActivityForResult(i, NEW_ITEM_REQUEST);
-                    }
-                });
+            //navega atraves do intent para o NewItemActivity
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this,NewItemActivity.class);
+                //executa o intent
+                startActivityForResult(i, NEW_ITEM_REQUEST);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == NEW_ITEM_REQUEST) {
+            if (resultCode == Activity.RESULT_OK) {
+                MyItem myItem = new MyItem();
+                myItem.title = data.getStringExtra("title");
+                myItem.description = data.getStringExtra("description");
+                myItem.photo = data.getData();
+
+                itens.add(myItem);
+            }
+        }
     }
 }
---
+++ app/src/main/java/lahass/stephany/lista/adapter/MyViewHolder.java
@@ -0,0 +1,13 @@
+package lahass.stephany.lista.adapter;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MyViewHolder extends RecyclerView.ViewHolder {
+    public MyViewHolder(@NonNull View itemView) {
+        super(itemView);
+    }
+}
+
